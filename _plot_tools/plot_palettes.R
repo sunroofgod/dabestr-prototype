f5b1e9629d260cccbb56004f4135446b6371cf3c
sunroofgod <e0725238@u.nus.edu>
2023-07-06 16:49:24 +0800

    feat: added adjusting colour palette function

---
+++ _plot_tools/plot_palettes.R
@@ -0,0 +1,48 @@
+# Applies palettes to <ggplot> objects
+apply_palette <- function(ggplot_object, palette_name) {
+  ggplot_object <- switch(
+    palette_name,
+    
+    "npg" =
+      ggplot_object + scale_color_npg() + scale_fill_npg(),
+    
+    "aaas" = 
+      ggplot_object + scale_color_aaas() + scale_fill_aaas(),
+    
+    "nejm" = 
+      ggplot_object + scale_color_nejm() + scale_fill_nejm(),
+    
+    "lancet" = 
+      ggplot_object + scale_color_lancet() + scale_fill_lancet(),
+    
+    "jama" = 
+      ggplot_object + scale_color_jama() + scale_fill_jama(),
+    
+    "jco" = 
+      ggplot_object + scale_color_jco() + scale_fill_jco(),
+    
+    "ucscgb" = 
+      ggplot_object + scale_color_ucscgb() + scale_fill_ucscgb(),
+    
+    "d3" = 
+      ggplot_object + scale_color_d3() + scale_fill_d3(),
+    
+    "locuszoom" = 
+      ggplot_object + scale_color_locuszoom() + scale_fill_locuszoom(),
+    
+    "igv" = 
+      ggplot_object + scale_color_igv() + scale_fill_igv(),
+    
+    "cosmic" = 
+      ggplot_object + scale_color_cosmic() + scale_fill_cosmic(),
+    
+    "uchicago" = 
+      ggplot_object + scale_color_uchicago() + scale_fill_uchicago(),
+    
+    "brewer" = 
+      ggplot_object + scale_color_brewer() + scale_fill_brewer()
+  )
+  
+  return(ggplot_object)
+}
+
