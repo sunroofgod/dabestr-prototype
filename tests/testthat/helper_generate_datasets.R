882f3014f5351b08427c2b0c5b8eccf8e78fc579
sunroofgod <e0725238@u.nus.edu>
2023-08-22 01:55:04 +0800

    update: datasets used & added testing for cohens_d and hedges_g

--- tests/testthat/helper_generate_datasets.R
+++ tests/testthat/helper_generate_datasets.R
@@ -1,6 +1,9 @@
 generate_non_proportional_dataset <- function(N = 40, seed = 12345) {
-  set.seed(seed)
+  set.seed(seed) # Fix the seed so the results are replicable.
+  # pop_size = 10000 # Size of each population.
+  N = 20 # The number of samples taken from each population
   
+  # Create samples
   c1 <- rnorm(N, mean = 3, sd = 0.4)
   c2 <- rnorm(N, mean = 3.5, sd = 0.75)
   c3 <- rnorm(N, mean = 3.25, sd = 0.4)
@@ -8,84 +11,93 @@ generate_non_proportional_dataset <- function(N = 40, seed = 12345) {
   t1 <- rnorm(N, mean = 3.5, sd = 0.5)
   t2 <- rnorm(N, mean = 2.5, sd = 0.6)
   t3 <- rnorm(N, mean = 3, sd = 0.75)
+  t4 <- rnorm(N, mean = 3.5, sd = 0.75)
+  t5 <- rnorm(N, mean = 3.25, sd = 0.4)
+  t6 <- rnorm(N, mean = 3.25, sd = 0.4)
   
+  # Add a `gender` column for coloring the data.
   gender <- c(rep('Male', N/2), rep('Female', N/2))
-  id <- 1: N
   
-  wide_data <- tibble::tibble(
-    Control1 = c1, Control2 = c2, Control3 = c3,
-    Test1 = t1, Test2 = t2, Test3 = t3, 
+  # Add an `id` column for paired data plotting.
+  id <- 1:N
+  
+  # Combine samples and gender into a DataFrame.
+  df <- tibble::tibble(
+    `Control 1` = c1, `Control 2` = c2, `Control 3` = c3,
+    `Test 1` = t1, `Test 2` = t2, `Test 3` = t3, `Test 4` = t4, `Test 5` = t5, `Test 6` = t6,
     Gender = gender, ID = id)
   
-  sample_tidy_dabestr_dataset <- wide_data %>%
+  df <- df %>%
     tidyr::gather(key = Group, value = Measurement, -ID, -Gender)
   
-  return(sample_tidy_dabestr_dataset)
+  return(df)
 }
 
 generate_proportional_dataset <- function(N = 40, seed = 12345) {
-  set.seed(seed)
+  set.seed(seed) # Fix the seed so the results are replicable.
+  N = 40 # The number of samples taken from each population
   
-  my.data.control_proportional <- data.frame(
-    Group = sample(c("Control1"), N, TRUE)
-  )
-  my.data.control_proportional$Success <- numeric(nrow(my.data.control_proportional))
-  my.data.control_proportional$Success[sample(nrow(my.data.control_proportional), 8)] <- 1
+  # Create samples
+  size = 1
+  c1 <- rbinom(N, size, prob = 0.2)
+  c2 <- rbinom(N, size, prob = 0.2)
+  c3 <- rbinom(N, size, prob = 0.8)
   
-  my.data.test_proportion1 <- data.frame(
-    Group = sample(c("Test1"), N, TRUE)
-  )
-  my.data.test_proportion1$Success <- numeric(nrow(my.data.test_proportion1))
-  my.data.test_proportion1$Success[sample(nrow(my.data.test_proportion1), 22)] <- 1
+  t1 <- rbinom(N, size, prob = 0.35)
+  t2 <- rbinom(N, size, prob = 0.2)
+  t3 <- rbinom(N, size, prob = 0.3)
+  t4 <- rbinom(N, size, prob = 0.4)
+  t5 <- rbinom(N, size, prob = 0.5)
+  t6 <- rbinom(N, size, prob = 0.6)
+  t7 <- c(rep(1, N))
   
-  my.data.test_proportion2 <- data.frame(
-    Group = sample(c("Test2"), N, TRUE)
-  )
-  my.data.test_proportion2$Success <- numeric(nrow(my.data.test_proportion2))
-  my.data.test_proportion2$Success[sample(nrow(my.data.test_proportion2), 15)] <- 1
+  # Add a `gender` column for coloring the data.
+  gender <- c(rep('Male', N/2), rep('Female', N/2))
   
-  my.data.test_proportion3 <- data.frame(
-    Group = sample(c("Test3"), N, TRUE)
-  )
-  my.data.test_proportion3$Success <- numeric(nrow(my.data.test_proportion3))
-  my.data.test_proportion3$Success[sample(nrow(my.data.test_proportion3), 18)] <- 1
+  # Add an `id` column for paired data plotting.
+  id <- 1:N
   
-  my.data.test_proportion4 <- data.frame(
-    Group = sample(c("Test4"), N, TRUE)
-  )
-  my.data.test_proportion4$Success <- numeric(nrow(my.data.test_proportion4))
-  my.data.test_proportion4$Success[sample(nrow(my.data.test_proportion4), 20)] <- 1
+  # Combine samples and gender into a DataFrame.
+  df <- tibble::tibble(
+    `Control 1` = c1, `Control 2` = c2, `Control 3` = c3,
+    `Test 1` = t1, `Test 2` = t2, `Test 3` = t3, `Test 4` = t4, `Test 5` = t5, 
+    `Test 6` = t6, `Test 7` = t7,
+    Gender = gender, ID = id)
   
-  my.data.test_proportion5 <- data.frame(
-    Group = sample(c("Control2"), N, TRUE)
-  )
-  my.data.test_proportion5$Success <- numeric(nrow(my.data.test_proportion5))
-  my.data.test_proportion5$Success[sample(nrow(my.data.test_proportion5), 4)] <- 1
+  df <- df %>%
+    tidyr::gather(key = Group, value = Success, -ID, -Gender)
   
-  my.data.proportional <- rbind(my.data.control_proportional, my.data.test_proportion1, my.data.test_proportion2, my.data.test_proportion3, my.data.test_proportion4, my.data.test_proportion5)
-  id <- data.frame(ID = rep(c(1:N), 6))
-  
-  my.data.proportional <- cbind(my.data.proportional, id)
+  return(df)
 }
 
 generate_deltadelta_dataset <- function(N = 40, seed = 12345) {
-  set.seed(seed)
-
+  set.seed(seed) # Fix the seed so the results are replicable.
+  # pop_size = 10000 # Size of each population.
+  N = 20 # The number of samples taken from each population
+  
+  # Create samples
   placebo <- rnorm(N, mean = 3, sd = 0.4)
   drug <- rnorm(N, mean = 3.5, sd = 0.75)
+  
+  # Add a `Genotype` column as the second variable
   genotype <- c(rep('M', N/2), rep('W', N/2))
-  id <- 1: N
+  
+  # Add an `id` column for paired data plotting.
+  id <- 1:N
+  
+  # Add a `Rep` column as the first variable for the 2 replicates of experiments done
   Rep <- rep(c("Rep1", "Rep2"), N/2)
   
-  wide_data <- tibble::tibble(
+  # Combine all columns into a DataFrame.
+  df <- tibble::tibble(
     Placebo = placebo,
     Drug = drug,
     Genotype = genotype, 
     ID = id,
     Rep = Rep)
   
-  sample_tidy_dabestr_dataset <- wide_data %>%
+  df <- df %>%
     tidyr::gather(key = Treatment, value = Measurement, -ID, -Genotype, -Rep)
   
-  return(sample_tidy_dabestr_dataset)
+  return(df)
 }
--- tests/testthat/test-001_load.R
+++ tests/testthat/test-001_load.R
@@ -1,11 +1,11 @@
 testthat::test_that("Able to load dataset", {
   np_dataset <- generate_non_proportional_dataset()
   expect_no_error(dabestr::load(np_dataset, x = Group, y = Measurement, 
-                                idx = c("Control1", "Test1")))
+                                idx = c("Control 1", "Test 1")))
   
   p_dataset <- generate_proportional_dataset()
   expect_no_error(dabestr::load(p_dataset, x = Group, y = Success, 
-                                idx = c("Control1", "Test1"), proportional = TRUE))
+                                idx = c("Control 1", "Test 1"), proportional = TRUE))
   
   dd_dataset <- generate_deltadelta_dataset()
   expect_no_error(dabestr::load(dd_dataset, x = Genotype, y = Measurement, 
@@ -17,39 +17,39 @@ testthat::test_that("Able to load dataset", {
 testthat::test_that("Able to detect non-valid params", {
   np_dataset <- generate_non_proportional_dataset()
   expect_error(dabestr::load(np_dataset, x = Grou, y = Measurement, 
-                             idx = c("Control1", "Test1")),
+                             idx = c("Control 1", "Test 1")),
                regexp = "Column x is not in data")
   expect_error(dabestr::load(np_dataset, x = Group, y = Measuremen, 
-                             idx = c("Control1", "Test1")),
+                             idx = c("Control 1", "Test 1")),
                regexp = "Column y is not in data")
   expect_error(dabestr::load(np_dataset, x = Group, y = Measurement, 
-                             idx = c("Control1", "Test1"), id_col = I),
+                             idx = c("Control 1", "Test 1"), id_col = I),
                regexp = "Column id_col is not in data")
   expect_error(dabestr::load(np_dataset, x = Group, y = Measurement, 
-                             idx = c("Control1", "Test1"), colour = Grou),
+                             idx = c("Control 1", "Test 1"), colour = Grou),
                regexp = "Column colour is not in data")
   expect_error(dabestr::load(np_dataset, x = Group, y = Measurement, 
-                             idx = c("Control1")),
+                             idx = c("Control 1")),
                regexp = "does not consist of at least 2 groups")
   expect_error(dabestr::load(np_dataset, x = Group, y = Measurement, 
-                             idx = list(c("Control1", "Test1"), c("Control2"))),
+                             idx = list(c("Control 1", "Test 1"), c("Control 2"))),
                regexp = "does not consist of at least 2 groups")
 })
 
 testthat::test_that("Able to detect non-valid params for proportional = TRUE", {
   np_dataset <- generate_non_proportional_dataset()
   expect_error(dabestr::load(np_dataset, x = Group, y = Measurement, 
-                             idx = c("Control1", "Test1"), proportional = TRUE),
+                             idx = c("Control 1", "Test 1"), proportional = TRUE),
                regexp = "data is not proportional")
 })
 
 testthat::test_that("Able to detect non-valid params for is_paired = TRUE", {
   np_dataset <- generate_non_proportional_dataset()
   expect_error(dabestr::load(np_dataset, x = Group, y = Measurement, 
-                             idx = c("Control1", "Test1", "Test2"), paired = "baseline"),
+                             idx = c("Control 1", "Test 1", "Test 2"), paired = "baseline"),
                regexp = "is TRUE but no id_col was supplied")
   expect_error(dabestr::load(np_dataset, x = Group, y = Measurement, 
-                             idx = c("Control1", "Test1", "Test2"), paired = "some",
+                             idx = c("Control 1", "Test 1", "Test 2"), paired = "some",
                              id_col = ID),
                regexp = "is not 'baseline' or 'sequential'.")
 })
@@ -57,17 +57,17 @@ testthat::test_that("Able to detect non-valid params for is_paired = TRUE", {
 testthat::test_that("Able to detect non-valid params for minimeta = TRUE", {
   p_dataset <- generate_proportional_dataset()
   expect_error(dabestr::load(p_dataset, x = Group, y = Success, 
-                             idx = c("Control1", "Test1"), id_col = ID, 
+                             idx = c("Control 1", "Test 1"), id_col = ID, 
                              proportional = TRUE, minimeta = TRUE),
                regexp = "proportional is TRUE but minimeta is also TRUE.")
   
   np_dataset <- generate_non_proportional_dataset()
   expect_error(dabestr::load(np_dataset, x = Group, y = Measurement, 
-                             idx = c("Control1", "Test1"),
+                             idx = c("Control 1", "Test 1"),
                              delta2 = TRUE, minimeta = TRUE),
                regexp = "delta2 is TRUE but minimeta is also TRUE.")
   expect_error(dabestr::load(np_dataset, x = Group, y = Measurement, 
-                             idx = c("Control1", "Test1", "Test2"), 
+                             idx = c("Control 1", "Test 1", "Test 2"), 
                              minimeta = TRUE),
                regexp = "minimeta is TRUE, but some idx does not consist of exactly 2 groups")
 })
@@ -75,7 +75,7 @@ testthat::test_that("Able to detect non-valid params for minimeta = TRUE", {
 testthat::test_that("Able to detect non-valid params for delta2 = TRUE", {
   p_dataset <- generate_proportional_dataset()
   expect_error(dabestr::load(p_dataset, x = Group, y = Success, 
-                             idx = c("Control1", "Test1"), id_col = ID, 
+                             idx = c("Control 1", "Test 1"), id_col = ID, 
                              proportional = TRUE, delta2 = TRUE),
                regexp = "delta2 is TRUE but proportional is also TRUE.")
 })
